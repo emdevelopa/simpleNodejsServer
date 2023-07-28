059b20e297fb8233f50b84d9ac72685caec3131f
emdevelopa <gyimahsunday@gmail.com>
2023-07-28 09:36:13 +0100

    Good  commit

--- index.js
+++ index.js
@@ -1,61 +1,50 @@
-const express = require("express")
+const http = require('http');
 const fs = require('fs');
 
-
-const app = express()
-
-const PORT = 3000
+const PORT = 3006;
 
 // middleware function that logs the request being made on the console
 const logIncomingRequests = (req, res, next) => {
-    console.log(`Received ${req.method} request at http://localhost:${PORT}${req.url}`);
-    next(); 
+  console.log(`Received ${req.method} request at http://localhost:${PORT}${req.url}`);
+  next();
 };
 
-//Using line 16, it will call the middleware function for all request made on any routes instead of specifying them one after the other  
-// app.use(logIncomingRequests)
-
-
-// middleware function to log request being made on this "/" route
-app.use("/",logIncomingRequests)
-
-//sendind a message "Hello node.js" to the / route which seems to be the base route
-app.get("/", (req, res) => {
-    res.send("Hello, Node.js")
-})
-
-
-// middleware function to log request being made on this "/file" route
-app.use("/file", logIncomingRequests)
-
-// reading content from data.txt file and sending the content to the "/file" route
-app.get("/file", (req, res) => {
-    fs.readFile("data.txt", "utf8", (error, data) => {
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    logIncomingRequests(req, res, () => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello, Node.js');
+    });
+  } else if (req.url === '/file') {
+    logIncomingRequests(req, res, () => {
+      fs.readFile('data.txt', 'utf8', (error, data) => {
         if (error) {
-            res.status(500).send('Error when trying to get content from data.txt file')
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Error when trying to get content from data.txt file');
         } else {
-            res.send(data)
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(data);
         }
-    })
-    
-})
-
-
-// middleware function to log request being made on this "/api/user" route
-app.use("api/user",logIncomingRequests)
-
-// sending a userObject to the /api/user route 
-app.get("/api/user", (req, res) => {
-    const userObject = {
+      });
+    });
+  } else if (req.url === '/api/user') {
+    logIncomingRequests(req, res, () => {
+      const userObject = {
         "name": "Gyimah Emmanuel",
         "email": "olatunbossemma17@gmail.com",
         "age": 19
-    }
-    res.json(userObject)
+      };
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(userObject));
+    });
+  } else {
+    logIncomingRequests(req, res, () => {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not found.');
+    });
+  }
+});
 
-})
-
-
-app.listen(PORT, () => {
-    console.log("Server running on port", PORT, `\nCheck it out here http://localhost:${PORT}`);
-})
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT, `\nCheck it out here http://localhost:${PORT}`);
+});
